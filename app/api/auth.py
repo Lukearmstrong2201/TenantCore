d3628b7aaf809b157165e56dbbabd0d7dcfc4674
Luke <lukearmstrong2201@hotmail.com>
2026-01-11 21:31:10 -0800

    Block authentication for inactive users

--- app/api/auth.py
+++ app/api/auth.py
@@ -16,7 +16,7 @@ async def login(
 ):
     user = await get_user_by_email(db, form_data.username)
 
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    if not user or not user.is_active or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
